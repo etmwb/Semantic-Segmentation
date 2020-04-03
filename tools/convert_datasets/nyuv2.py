9a6f5f5aec64707c7c4e0ced3ed389a597406838
etmwb <1026014376@qq.com>
2020-04-03 15:25:06 +0800

    Update nyuv2.py

--- tools/convert_datasets/nyuv2.py
+++ tools/convert_datasets/nyuv2.py
@@ -5,6 +5,7 @@ import h5py
 import argparse
 import numpy as np
 from PIL import Image 
+import scipy.io as sco
 
 from util import processDepthImage
 
@@ -12,9 +13,8 @@ def saveColor(data, out_dir):
     colors = np.array(data['images'])
     for i in range(len(colors)): 
         color = colors[i]
-        r, g, b = Image.fromarray(color[0]).convert('L'), Image.fromarray(color[1]).convert('L'), Image.fromarray(color[2]).convert('L')
-        color = Image.merge("RGB", (r, g, b)).transpose(Image.ROTATE_270)
-        color.save(os.path.join(out_dir, str(i)+'.png'))
+        color = np.stack((color[2].T, color[1].T, color[0].T), axis=-1)
+        cv2.imwrite(os.path.join(out_dir, str(i)+'.png'), color)
 
 def saveDepth(data, out_dir):
     depths = np.array(data['depths'])
@@ -22,12 +22,10 @@ def saveDepth(data, out_dir):
         depth = np.uint16(depths[i].T * 1000)
         cv2.imwrite(os.path.join(out_dir, str(i)+'.png'), depth)
 
-def saveLabel(data, out_dir):
-    labels = np.array(data['labels'])
-    for i in range(len(labels)): 
-        label = labels[i]
-        label = Image.fromarray(np.uint8(label)).transpose(Image.ROTATE_270)
-        label.save(os.path.join(out_dir, str(i)+'.png'))
+def saveLabel(labels, out_dir):
+    for i in range(labels.shape[-1]): 
+        label = labels[:, :, i]
+        cv2.imwrite(os.path.join(out_dir, str(i)+'.png'), label)
 
 def camera_matrix(): 
     fx_rgb = 5.1885790117450188e+02
@@ -38,12 +36,13 @@ def camera_matrix():
     return C 
 
 # This function is borrowed from https://github.com/charlesCXK/Depth2HHA-python
-def saveHHA(files, out_dir): 
+def saveHHA(data, out_dir): 
     C = camera_matrix()
-    for i in range(len(files)): 
-        D = RD = cv2.imread(os.path.join(out_dir, 'depth', files[i]), cv2.COLOR_BGR2GRAY)/1000
-        missingMask = (RD == 0);
-        pc, N, yDir, h, pcRot, NRot = processDepthImage(D * 100, missingMask, C);
+    depths = np.array(data['depths'])
+    for i in range(1): 
+        D = RD = depths[i].T
+        missingMask = (RD == 0)
+        pc, N, yDir, h, pcRot, NRot = processDepthImage(D * 100, missingMask, C)
     
         tmp = np.multiply(N, yDir)
         acosValue = np.minimum(1,np.maximum(-1,np.sum(tmp, axis=2)))
@@ -82,6 +81,8 @@ def saveHHA(files, out_dir):
 def parse_args(): 
     parser = argparse.ArgumentParser(description='DetSeg extract nyuv2 dataset')
     parser.add_argument('matfile', help='nyuv2_depth_v2_labeled.mat')
+    # download labels40.mat from https://github.com/ankurhanda/nyuv2-meta-data
+    parser.add_argument('labelfile', help='labels40.mat')
     parser.add_argument('--out_dir', default='data/nyuv2', help='data dir')
     args = parser.parse_args()
     return args 
@@ -89,12 +90,13 @@ def parse_args():
 if __name__ == '__main__': 
     args = parse_args()
     data = h5py.File(args.matfile)
+    labels = sco.loadmat(args.labelfile)['labels40']
     if not os.path.exists(args.out_dir): 
         for t in ['image', 'depth', 'label', 'HHA']: 
             os.makedirs(os.path.join(args.out_dir, t))
     
     saveColor(data, os.path.join(args.out_dir, 'image'))
     saveDepth(data, os.path.join(args.out_dir, 'depth'))
-    saveLabel(data, os.path.join(args.out_dir, 'label'))
-    saveHHA(os.listdir(os.path.join(args.out_dir, 'image')), args.out_dir)
+    saveLabel(labels, os.path.join(args.out_dir, 'label'))
+    saveHHA(data, args.out_dir)
     
