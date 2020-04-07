abc068db72781371ac97a2c05e4b4d8e037a4103
etmwb <1026014376@qq.com>
2020-04-07 21:24:29 +0800

    Create __init__.py

---
+++ detseg/ops/set_abstraction/__init__.py
@@ -0,0 +1,7 @@
+from .set_abstraction import (furthest_point_sample, gather_operation, 
+                              three_nn, three_interpolate, QueryAndGroup, GroupAll)
+
+__all__ = [
+    "furthest_point_sample", "gather_operation", 
+    "three_nn", "three_interpolate", "QueryAndGroup", "GroupAll"
+]
