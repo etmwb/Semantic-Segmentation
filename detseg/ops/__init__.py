42a9a0857dcec3d075366c4bbf0a35078fefb15e
etmwb <1026014376@qq.com>
2020-04-07 21:28:02 +0800

    Update __init__.py

--- detseg/ops/__init__.py
+++ detseg/ops/__init__.py
@@ -2,10 +2,13 @@ from .context_block import ContextBlock
 from .dcn import (DeformConv, DeformConvPack, ModulatedDeformConv,
                   ModulatedDeformConvPack, DepthawareConv, DepthDeformConvPack,
                   deform_conv, modulated_deform_conv, depthaware_conv)
+from .set_abstraction import (furthest_point_sample, gather_operation, 
+                              three_nn, three_interpolate, QueryAndGroup, GroupAll)
 
 __all__ = [
     'DeformConv', 'DeformConvPack', 'ModulatedDeformConv',
     'ModulatedDeformConvPack', 'DepthawareConv', 'DepthDeformConvPack',
     'deform_conv', 'modulated_deform_conv', 'depthaware_conv',
-    'ContextBlock'
+    'ContextBlock', 'furthest_point_sample', 'gather_operation', 
+    'three_nn', 'three_interpolate', 'QueryAndGroup', 'GroupAll'
 ]
