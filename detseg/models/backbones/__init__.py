c3c592e76c4dfb1536b378b1350de414085f6b9d
etmwb <1026014376@qq.com>
2020-04-22 11:25:52 +0800

    Update __init__.py

--- detseg/models/backbones/__init__.py
+++ detseg/models/backbones/__init__.py
@@ -1,4 +1,5 @@
 from .resnet import ResNet, make_res_layer
 from .pointnet import PointNet
+from .pointnet2 import PointNet2
 
-__all__ = ['ResNet', 'make_res_layer', 'PointNet']
+__all__ = ['ResNet', 'make_res_layer', 'PointNet', 'PointNet2']
